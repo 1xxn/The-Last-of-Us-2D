0675b3cd2635e55befe659c2851fa043efcc957d
leon :^) <98491048+corex3@users.noreply.github.com>
2023-02-20 18:25:46 +0100

    Setting up libGDX project

---
+++ desktop/src/com/corex/thelastofus2d/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.corex.thelastofus2d;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.corex.thelastofus2d.TheLastOfUs2D;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("The Last of Us 2D");
+		new Lwjgl3Application(new TheLastOfUs2D(), config);
+	}
+}
---
+++ html/src/com/corex/thelastofus2d/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.corex.thelastofus2d.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.corex.thelastofus2d.TheLastOfUs2D;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TheLastOfUs2D();
+        }
+}
